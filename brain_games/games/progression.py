7576d5aacddce0040fd1862c55fe152fd2c6e964
bloodywd <bloodyforreal@gmail.com>
2023-10-31 06:34:26 +0300

    updated progression generator

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,18 +5,12 @@ DESCRITION = 'What number is missing in the progression?'
 
 
 def get_round():
-    next_number = randint(1, 10)
-    step = randint(2, 8)
-    progression_range = randint(5, 10)
-    answer_index = randint(0, progression_range - 1)
-    question = ''
-    right_answer = 0
-    for i in range(0, progression_range):
-        if i == answer_index:
-            question += ".. "
-            right_answer = next_number
-            next_number += step
-            continue
-        question = question + str(next_number) + " "
-        next_number += step
-    return (question.strip(), str(right_answer))
+    first_element = randint(1, 10)  # первый элемент прогрессии
+    step = randint(2, 8)  # шаг между элементами прогрессии
+    progression_range = randint(5, 10)  # количество элементов в прогрессии
+    progression = [
+        str(first_element + i * step) for i in range(progression_range)
+    ]  # генерируем прогрессию
+    answer_index = randint(0, progression_range - 1)  # скрытый элемент
+    answer, progression[answer_index] = progression[answer_index], '..'
+    return (' '.join(progression), str(answer))
