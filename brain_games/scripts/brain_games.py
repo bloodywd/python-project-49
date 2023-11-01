74514349b47d45c07367bd54f26519b12bd8bd99
bloodywd <bloodyforreal@gmail.com>
2023-11-01 10:52:26 +0300

    trying to remove duplications

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
+from ..cli import welcome_user
 
 
 def main():
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from brain_games.engine import play_game
-from brain_games.games import progression
+from ..engine import play_game
+from ..games import progression
 
 
 def main():
